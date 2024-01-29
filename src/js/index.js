827defa8bb548b5788c5833a6631ef65848739d3
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-01-29 12:56:23 -0300

    finalizando o projeto

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
-try {
-    
-} catch (error) {
-    
-}
+const botao = document.querySelector(".btn-plataforma")
+
+const elementosPlataformas = document.querySelector(".btn-plataforma .plataformas")
+
+botao.addEventListener("click" ,()=>{
+    elementosPlataformas.classList.toggle('ativo')
+})
